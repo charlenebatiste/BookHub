7740e059d02791faaea235e5b394da4f226e36e0
charlenebatiste <batistecharlene@gmail.com>
2021-05-18 12:11:32 -0400

    reseeded user info and tested associations

--- dbTest.js
+++ dbTest.js
@@ -15,12 +15,42 @@ function seeAUsersPosts(){
         include: [db.post]
     })
     .then (thisUser => {
-        console.log(thisUser);
-        console.log(thisUser.dataValues.posts);
+        console.log(thisUser.name);
+        console.log(thisUser.posts);
     })
     .catch(err => {
         console.log(err)
     })
 }
 
-// seeAUsersPosts()
+seeAUsersPosts()
+
+function findComments(){
+    db.comment.findAll({
+        where:{postId: 2}
+    })
+    .then( allComments => {
+        console.log(allComments)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+// findComments()
+
+function findPostAndComments(){
+    db.post.findOne({
+        where: {userId:4},
+        include: [db.comment]
+    })
+    .then(postWithComments => {
+        console.log(postWithComments.title)
+        console.log(postWithComments.comments[0].content)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+// findPostAndComments()
